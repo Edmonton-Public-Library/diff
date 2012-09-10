3c580937d9aa03874885cb46f82b8fe5c4cfd11c
Andrew Nisbet <anisbet@epl.ca>
2012-09-10 15:09:51 -0600

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,13 @@
+# copies most rescent files from eplapp for updating to git.
+SERVER=eplapp.library.ualberta.ca
+USER=sirsi
+REMOTE=~/Unicorn/EPLwork/anisbet/
+LOCAL=~/projects/diff/
+APP=diff.pl
+
+put: test
+	#scp ${LOCAL}${APP} ${USER}@${SERVER}:${REMOTE}
+get:
+	scp ${USER}@${SERVER}:${REMOTE}${APP} ${LOCAL}
+test:
+	perl -c ${LOCAL}${APP}
---
+++ diff.pl
@@ -0,0 +1,209 @@
+#!/usr/bin/perl -w
+###########################################################################
+# Purpose: Diff files with logical operators.
+# Method:  Use reduced boolean algerbra set to diff files.
+#
+# Author:  Andrew Nisbet, Edmonton Public Library
+# Date:    September 10, 2012
+# Rev:     0.0 - Dev.
+###########################################################################
+
+use strict;
+use warnings;
+use vars qw/ %opt /;
+use Getopt::Std;
+use Switch;
+
+my @LHS      = ();
+my @RHS      = ();
+
+#
+# Message about this program and how to use it
+#
+sub usage()
+{
+    print STDERR << "EOF";
+
+	usage: $0 [-x]
+This script allows the user to specify differences in files by boolean algerbra.
+Example: echo "file1.txt or file2.txt" | diff.pl would output the contents of both files.
+Example: echo "file1.txt and file2.txt" | diff.pl would output lines that match both files.
+         echo "file1.txt not file2.txt" | diff.pl outputs lines from file1.txt that are not in file2.txt
+ -d: Print debug information.
+ -i: Ignore letter casing.
+ -o: Order all input file contents first.
+ -x: This (help) message.
+
+example: $0 -x
+
+EOF
+    exit;
+}
+
+# Kicks off switch setting.
+# param:  
+# return: 
+sub init
+{
+    my $opt_string = 'diox';
+    getopts( "$opt_string", \%opt ) or usage();
+    usage() if ( $opt{'x'} );
+	my $sentence = <>;
+	parse( $sentence );
+}
+init();
+
+# Returns a list of items anded from LHS and RHS.
+# param:  
+# return: list of items.
+sub sAnd
+{
+	my $tmp;
+	while ( @RHS )
+	{
+		my $value = shift( @RHS );
+		foreach my $test ( @LHS )
+		{
+			if ( $test ne $value )
+			{
+				next;
+			}
+			else
+			{
+				$tmp->{$value} = 1;
+			}
+		}
+	}
+	return keys %$tmp;
+}
+
+# Returns a list of uniq items that are in LHS or RHS.
+# param:  
+# return: list of items.
+sub sOr
+{
+	
+	return ();
+}
+
+# Could have named this better, but returns a list of items from LHS that are not in RHS
+# param:  
+# return: list of items.
+sub sNot
+{
+	my $tmp;
+	while ( @RHS )
+	{
+		my $value = shift( @RHS );
+		foreach my $test ( @LHS )
+		{
+			if ( $test eq $value )
+			{
+				next;
+			}
+			else
+			{
+				print "$test ne $value";
+				$tmp->{$test} = 1;
+				last;
+			}
+		}
+	}
+	return keys %$tmp;
+}
+
+#
+# Performs the operation designated by arguments on left hand side and right hand side lists.
+# param:  operation string - either 'and', 'or', or 'not'.
+# return: list after the operation has completed.
+sub doOperation
+{
+	my ( $operation ) = @_;
+	print "operation: '$operation'    LHS=\n @LHS     RHS=\n @RHS" if ( $opt{'d'} );
+	switch ( $operation )
+	{
+		case "NOT" { return sNot( ); }
+		case "AND" { return sAnd( ); }
+		case "OR"  { return sOr( ); }
+		else
+		{
+			print STDERR "Unknown operation '$operation'\n";
+			exit( 0 );
+		}
+	}
+}
+
+
+#
+# Parses the grammer of the input line.
+#
+# Syntax:
+# STATEMENT: ( EXPRESSION ) OPERATOR ( EXPRESSION )
+# EXPRESSION: FILE OPERATOR FILE
+# OPERATOR: and, or, not
+# FILE: text file name
+#
+# param:  sentence string.
+# return: 
+sub parse
+{
+	# legal tokens are '(', ')', 'and', 'or', 'not', <file name>.
+	my @tokens   = split( /\s/, $_[0] );
+	my $operator = "";
+	while (@tokens)
+	{
+		my $token = shift( @tokens );
+		if ( $token eq "or" ) # only on FILE or after CLOSE_PAREN
+		{
+			print "or: '$token'\n" if ( $opt{'d'} );
+			$operator = "OR";
+		}
+		elsif ( $token eq "and" ) # only on FILE or after CLOSE_PAREN
+		{
+			print "and: '$token'\n" if ( $opt{'d'} );
+			$operator = "AND"; 
+		}
+		elsif ( $token eq "not" ) # only on FILE or after CLOSE_PAREN
+		{
+			print "not: '$token'\n" if ( $opt{'d'} );
+			$operator = "NOT";
+		}
+		# elsif ( $token eq "(" ) # only on INIT or after OPERATOR
+		# {
+			# print "(: '$token'\n" if ( $opt{'d'} );
+			## what to do here? can we call parse recursively?
+		# }
+		# elsif ( $token eq ")" ) # can only follow FILE
+		# {
+			# print "): '$token'\n" if ( $opt{'d'} );
+			# @LHS = doOperation( $operation, @LHS, @RHS );
+		# }
+		elsif ( -T $token ) # the token looks like a text file.
+		{
+			print "file: '$token'\n" if ( $opt{'d'} );
+			open( FILE_IN, "<$token" ) or die "Error reading '$token': $!\n";
+			if ( not @LHS )
+			{
+				@LHS = <FILE_IN>;
+				close( FILE_IN );
+				next;
+			}
+			@RHS = <FILE_IN>;
+			close( FILE_IN );
+			@LHS = doOperation( $operator );
+		}
+		else
+		{
+			print STDERR "Syntax error: unrecognized token '$token'.\n";
+			exit( 0 );
+		}
+	}
+	
+
+	foreach my $result ( @LHS )
+	{
+		print "$result" if ( $result );
+	}
+}
+
+1;
